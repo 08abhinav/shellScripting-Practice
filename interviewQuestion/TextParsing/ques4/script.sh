608fab689c43dca38fb3671ddbaa18d2ca840cca
08abhinav <abhinavnegi101@gmail.com>
2026-01-07 18:27:05 +0530

    script.sh

---
+++ interviewQuestion/TextParsing/ques4/script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+##################################################################################################
+# Author: Abhinav Negi
+# Question: Print the characters from thirteenth position to the end.
+#
+# How to run the shell bash script in terminal
+# You can type ./<file-name> (But it will give you permission denied error, so resolve using 
+# chmod +x <file-name> and then try)
+#####################################################################################################
+cut -c13- interviewQuestion/TextParsing/ques4/file.txt
