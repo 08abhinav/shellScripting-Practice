077dd32dbd57b1b10103df5b02952c5cb6fa0b7d
08abhinav <abhinavnegi101@gmail.com>
2026-02-21 10:27:46 +0530

    recursive command too

--- interviewQuestion/Intermediate/ques1/script.sh
+++ interviewQuestion/Intermediate/ques1/script.sh
@@ -8,4 +8,9 @@
 # chmod +x <file-name> and then try)
 #####################################################################################################
 
-find /c/Users/abhin/DSA/shell/interviewQuestion/Intermediate/ques1 -type f -name "*.txt" | wc -l
+# This command will count all the .txt file inside current directory 
+# find /c/Users/abhin/DSA/shell/interviewQuestion/Intermediate/ques1 -type f -name "*.txt" | wc -l
+
+# This command will count recursively including subdirectories
+find . -type f -name "*.txt" | wc -l
+
