2109a3e3e918d086a63ba69085c95e006dd0ee91
08abhinav <abhinavnegi101@gmail.com>
2026-02-21 10:31:24 +0530

    added a new command

--- interviewQuestion/Intermediate/ques1/script.sh
+++ interviewQuestion/Intermediate/ques1/script.sh
@@ -8,9 +8,14 @@
 # chmod +x <file-name> and then try)
 #####################################################################################################
 
+set -ex
+
 # This command will count all the .txt file inside current directory 
-# find /c/Users/abhin/DSA/shell/interviewQuestion/Intermediate/ques1 -type f -name "*.txt" | wc -l
+find /c/Users/abhin/DSA/shell/interviewQuestion/Intermediate/ques1 -type f -name "*.txt" | wc -l
 
 # This command will count recursively including subdirectories
 find . -type f -name "*.txt" | wc -l
+
+# Case-insensitive command
+find . -type f -iname "*.txt" | wc -l
 
